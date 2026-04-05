03c72856c40ff08c26c66115cf1d9fc4d1992f15
Gokila P <pgokila403@gmail.com>
2026-04-06 00:21:55 +0530

    updates 5

--- client/src/constants/apiPaths.js
+++ client/src/constants/apiPaths.js
@@ -1,4 +1,4 @@
-const apiBase = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+const apiBase = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 export const API_BASE_URL = apiBase;
 
